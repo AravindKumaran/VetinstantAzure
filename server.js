b4ef756a5cf8ef30c0b6e8ff64491aa1bac21818
Minazuddin <minhaajudeen@vinteksolution.com>
2021-07-20 11:23:58 +0530

    public folder

--- server.js
+++ server.js
@@ -126,7 +126,7 @@ if (process.env.NODE_ENV === "development") {
 // app.use(morgan('combined', { stream: logs }))
 
 app.use(express.static(path.join(__dirname, "public/uploads")));
-app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+app.use("/public", express.static(path.join(__dirname, "public/uploads")));
 
 // Mounting routes
 
